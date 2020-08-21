dcad23b4f20f5000446bb6d921bbf4808733c9af
Kotori4 <68325394+Kotori4@users.noreply.github.com>
2020-08-21 21:02:14 +0900

    お勧めフード投稿用コントローラー設定

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,6 +1,23 @@
 class FoodsController < ApplicationController
 
   def index
-    
+  end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.new(food_params)
+    if @food.save
+      redirect_to foods_path   
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def food_params
+    params.require(:food).permit(:food_title, :food_text).merge(user_id: current_user.id)
   end
 end
