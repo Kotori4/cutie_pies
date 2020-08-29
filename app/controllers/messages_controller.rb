eb92e6eae07e50035b87bca1a3bc4375772f4b6e
Kotori4 <68325394+Kotori4@users.noreply.github.com>
2020-08-29 11:33:32 +0900

    アクション設定

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,17 +1,9 @@
 class MessagesController < ApplicationController
-
-  # def index
-  #   # @messages = @room..messages.includes(:user)
-  #   @message = Message.new.includes(:user)
-  #   @room = Room.find(params[:room_id])
-  #   @messages = @room.messages.includes(:user)
-  # end
-
+  
   def new
   end
 
   def create
-    binding.pry
     @room = Room.find(params[:room_id])
     @message = @room.messages.create!(message_params)
     ActionCable.server.broadcast 'room_channel', message: @message.content
