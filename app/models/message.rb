c345629974190c7fad9122ee5f8665e000d0b910
Kotori4 <68325394+Kotori4@users.noreply.github.com>
2020-08-29 11:33:24 +0900

    ビューファイルの追加

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,11 +1,6 @@
 class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
-  # has_one_attached :chat_image
   validates :content, presence: true
-  # , unless: :was_attached?
 
-  # def was_attached?
-  #   self.image.attached?
-  # end
 end
