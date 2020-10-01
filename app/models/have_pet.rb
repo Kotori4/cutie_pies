8db59dda8c484e1295b0114c4587d89d2b8aff6c
Kotori4 <68325394+Kotori4@users.noreply.github.com>
2020-10-01 17:04:41 +0900

    ペットの有無項目追加

--- app/models/have_pet.rb
+++ app/models/have_pet.rb
@@ -1,9 +1,13 @@
 class HavePet < ActiveHash::Base
   self.data = [
     { id: 1, name: '猫がいる！' },
-    { id: 2, name: '犬がいる！' },
-    { id: 3, name: '両方いる！' },
-    { id: 4, name: '別の動物がいる！' },
-    { id: 5, name: 'いない！悲しい！' }
+    { id: 2, name: '猫がいた！' },
+    { id: 3, name: '犬がいる！' },
+    { id: 4, name: '犬がいた！' },
+    { id: 5, name: '両方いる！' },
+    { id: 6, name: '両方いた！' },
+    { id: 7, name: '別の動物がいる！' },
+    { id: 8, name: '別の動物がいた！' },
+    { id: 9, name: 'いない！悲しい！' }
   ]
 end
