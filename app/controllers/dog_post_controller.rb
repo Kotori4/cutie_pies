8a22a92168096d0af9ab5058cce20560538466e7
Kotori4 <68325394+Kotori4@users.noreply.github.com>
2020-10-06 13:21:16 +0900

    写真投稿機能の実装

--- app/controllers/dog_post_controller.rb
+++ app/controllers/dog_post_controller.rb
@@ -1,6 +1,31 @@
 class DogPostController < ApplicationController
-
+  skip_before_action :authenticate_user!, only: [:index]
+  before_action :all_posted, only: [:index, :posted]
+  
   def index
   end
+
+  def new
+    @dog_post = DogPost.new
+  end
+
+  def create
+    @dog_post = DogPost.new(dog_post_params)
+    if @dog_post.save
+      redirect_to dog_post_index_path   
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def all_posted
+    @dog_posts = DogPost.all.includes(:user).order("created_at DESC").page(params[:page]).per(24)
+  end
   
-end
+    def dog_post_params
+      params.require(:dog_post).permit(:dog_image, :dog_post_title, :dog_post_text).merge(user_id: current_user.id)
+    end
+  end
+  
