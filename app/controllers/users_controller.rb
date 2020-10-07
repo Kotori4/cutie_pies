396c59d40dddd94936c785faba8a755fef718659
Kotori4 <68325394+Kotori4@users.noreply.github.com>
2020-10-08 08:14:06 +0900

    マイページに犬の投稿写真の表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @cat_posts = @user.cat_posts.includes(:user).order("created_at DESC")
+    @dog_posts = @user.dog_posts.includes(:user).order("created_at DESC")
     @items = @user.items.includes(:user).order("created_at DESC")
   end
 end
