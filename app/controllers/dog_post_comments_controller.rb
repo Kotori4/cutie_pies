68e2548aa04a5cbce4752d24fe3c5a8dc4839475
Kotori4 <68325394+Kotori4@users.noreply.github.com>
2020-10-07 20:39:37 +0900

    コメント投稿機能の実装

--- app/controllers/dog_post_comments_controller.rb
+++ app/controllers/dog_post_comments_controller.rb
@@ -1,3 +1,13 @@
 class DogPostCommentsController < ApplicationController
+  def create
+    @dog_post_comment = current_user.dog_post_comments.build(dog_comment_params)
+    if @dog_post_comment.save
+      redirect_to "/dog_post/#{@dog_post_comment.dog_post.id}"
+    end
+  end
 
+  private
+  def dog_comment_params
+    params.require(:dog_post).permit(:dog_post_comment).merge(dog_post_id: params[:dog_post_id])
+  end
 end
